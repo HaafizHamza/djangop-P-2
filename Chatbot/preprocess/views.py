ca8f7fadc393477cf5e318fad9cf46e5c7aa05c8
Hamza <qomaise@outlook.com>
2022-05-22 11:36:28 +0500

    Function based Views

--- Chatbot/preprocess/views.py
+++ Chatbot/preprocess/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse,HttpResponseNotFound
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
-
+from django.core.paginator import Paginator
 def index(request):
     return HttpResponse("This is first page of our bot site")
 
@@ -9,7 +10,14 @@ def index(request):
 @csrf_exempt
 @require_http_methods(['GET'])
 def detail(request):
+    items=("train","models","chat","tkinter","good","happy")
     if request.method=='GET':
-        return HttpResponse("This is dtail page of our bot site")
+        paginator= Paginator(items,2)
+        pages=request.GET.get('page',1)
+        try:
+            items=paginator.page(pages)
+        except PageNotAnInteger:
+            items=paginator.page(1)
+        return render(request,'preprocess/list.html',{'items':items})
     elif request.method=='POST':
         return HttpResponseNotFound("Post is not allowed")
